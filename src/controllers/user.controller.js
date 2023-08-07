f1556deb7a00e8a6613b067d380f7f7ee3a158ab
ricarvsan <ricarvsan@gmail.com>
2023-08-07 15:37:03 -0300

    get: users/me

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,16 @@
 import bcrypt from 'bcrypt';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 import { db } from '../database/database.connection.js';
 
 export async function signup(req, res) {
-    const {name, email, password, confirmPassword} = req.body;
+    const { name, email, password, confirmPassword } = req.body;
 
-    if(password !== confirmPassword) return res.status(422).send({message: 'Os passwords não são iguais!'})
+    if (password !== confirmPassword) return res.status(422).send({ message: 'Os passwords não são iguais!' })
 
     try {
         //const emailInUse = await db.collection('users').findOne({email});
         const emailInUse = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
-        if(emailInUse.rowCount) return res.status(409).send({message: 'Já existe um usuário com este email cadastrado!'})
+        if (emailInUse.rowCount) return res.status(409).send({ message: 'Já existe um usuário com este email cadastrado!' })
 
 
         const hash = bcrypt.hashSync(password, 10)
@@ -27,36 +27,49 @@ export async function signup(req, res) {
 }
 
 export async function signin(req, res) {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     try {
         //const user = await db.collection('users').findOne({email});
         const user = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
-        if(!(user.rowCount)) return res.status(401).send({message: 'Usuário não encontrado!'});
-        
+        if (!(user.rowCount)) return res.status(401).send({ message: 'Usuário não encontrado!' });
+
         const correctPassword = bcrypt.compareSync(password, user.rows[0].password);
-        if(!correctPassword) return res.status(401).send({message: 'Senha incorreta!'});
+        if (!correctPassword) return res.status(401).send({ message: 'Senha incorreta!' });
 
         const token = uuid();
 
         //await db.collection('session').deleteMany({userId: user._id});
         await db.query(`DELETE FROM session WHERE "userId" = $1`, [user.rows[0].id])
-        
+
         //await db.collection('session').insertOne({token, userId: user._id});
         await db.query(`INSERT INTO session ("userId", token) VALUES ($1, $2)`, [user.rows[0].id, token])
 
-        res.status(200).send({token})
+        res.status(200).send({ token })
     } catch (err) {
         res.status(500).send(err.message)
     }
 }
 
 export async function aboutMe(req, res) {
-
-
     try {
+        const me = await db.query(`
+            SELECT users.id AS id, users.name AS name, SUM(visitants) AS "visitCount" FROM session
+            JOIN users ON session."userId" = users.id
+            JOIN "middleUrls" ON users.id = "middleUrls"."userId"
+            JOIN urls ON "middleUrls"."urlsId" = urls.id
+            WHERE users.id = $1
+            GROUP BY users.id;
+        `, [res.locals.session.rows[0].userId]);
 
-        res.status(200).send({})
+        const urls = await db.query(`
+            SELECT urls.id, urls."shortUrl", urls.url, urls.visitants AS "visitCount" FROM "middleUrls"
+            JOIN urls ON "middleUrls"."urlsId" = urls.id
+            WHERE "middleUrls"."userId" = $1;
+        `, [res.locals.session.rows[0].userId]);
+
+        res.status(200).send({...me.rows[0], shortenedUrls: urls.rows})
+
     } catch (err) {
         res.status(500).send(err.message)
     }
